ec6f133c2b0ae54f3a3ffac02f83fd2c8b728116
Wolf McNally <wolf@wolfmcnally.com>
2019-10-21 13:30:41 -0700

    Added minimum platform requirements.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "WolfLorem",
+    platforms: [
+        .iOS(.v9), .macOS(.v10_13), .tvOS(.v11)
+    ],
     products: [
         .library(
             name: "WolfLorem",
