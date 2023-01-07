f2f6258bbd4df953036cd3a8689047ee502b1c1f
Wolf McNally <wolf@wolfmcnally.com>
2023-01-07 15:18:22 -0800

    Removed platform requirements.

--- Package.swift
+++ Package.swift
@@ -3,19 +3,16 @@ import PackageDescription
 
 let package = Package(
     name: "WolfLorem",
-    platforms: [
-        .iOS(.v11), .macOS(.v10_13), .tvOS(.v11)
-    ],
     products: [
         .library(
             name: "WolfLorem",
             targets: ["WolfLorem"]),
-        ],
+    ],
     dependencies: [
     ],
     targets: [
         .target(
             name: "WolfLorem"
-          )
-        ]
+        )
+    ]
 )
